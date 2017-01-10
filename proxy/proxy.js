4dd2f5da5b0610b000ec2a45aa1f40db2ec16222
Thibault Friedrich <tf@elqui.fr>
2017-01-10 23:46:13 +0100

    add require for exec

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const path = require('path');
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 require('dotenv').config();
 
